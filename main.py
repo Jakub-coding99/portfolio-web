f0178b6855d30bf049347495810d10bf9ccf7315
Jakub Večeřa <kuba.vecera@email.cz>
2025-10-28 19:51:40 +0100

    created admin pannel, sending emails from contact form works

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI,Request,Depends ,File, Form, UploadFile
+from fastapi import FastAPI,Request,Depends ,File, Form, UploadFile, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from typing import Annotated
-import secrets
+from passlib.context import CryptContext
 from database import Projects, Admin,create_db_and_tables,engine,Session,select
 import os
 from typing import List
+from fastapi.responses import RedirectResponse
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from dotenv import load_dotenv,find_dotenv
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
 
 
 app = FastAPI()
@@ -16,45 +23,99 @@ security = HTTPBasic()
 
 create_db_and_tables()
 
+@app.get("/user/admin")
+def get_admin(credentials : Annotated[HTTPBasicCredentials,Depends(security)]):
+        with Session(engine) as session:
+            hasher = CryptContext(schemes=["bcrypt"], deprecated="auto")
+            admin = session.scalars(select(Admin)).first()
+            check_pass = hasher.verify(credentials.password,admin.password)
 
-@app.post("/test",response_class = HTMLResponse)
-async def  test(request : Request,title:str = Form(...),description:str = Form(...),files: List[UploadFile] = Form(...),preview:str = Form(...)):
-    img_path = []
-    
-    for file in files:
-        UPLOAD_DIR = "static/img/"
-        os.makedirs(UPLOAD_DIR,exist_ok=True)
-        location_file = os.path.join(UPLOAD_DIR,file.filename)
-    
-        with open (location_file, "wb") as f:
-            content = await file.read()
-            f.write(content)
+            if admin.user_name == credentials.username and check_pass:
+                return RedirectResponse(url="/admin-section")
+            else:
         
-        img_path.append(location_file)
+                raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Unauthorized",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+
+
+@app.post("/admin-section",response_class = HTMLResponse,)
+async def post_admin_pannel(request : Request,title:str = Form(...),description:str = Form(...),
+                files: List[UploadFile] = File(None),preview:str = Form(...),admin=Depends(get_admin)):
+    img_path = []
+    projects = all_project()
+    
+    
+    if files:
+        for file in files:
+            if not file.filename:
+                fallback_img = "static/img/no-img.jpg"
+                img_path.append(fallback_img)
+                continue
+       
+            UPLOAD_DIR = "static/img/"
+            os.makedirs(UPLOAD_DIR,exist_ok=True)
+            location_file = os.path.join(UPLOAD_DIR,file.filename)
+        
+            with open (location_file, "wb") as f:
+                content = await file.read()
+                f.write(content)
+            
+            img_path.append(location_file)
     
     
     new_project = Projects(title=title, description=description,image_url=img_path,preview=preview)
     with Session(engine) as session:
         session.add(new_project)
         session.commit()
+        return RedirectResponse("/admin-section", status_code=303)
+
+        
+
+    return templates.TemplateResponse("save_project.html", {"request":request,"filename": [f.filename for f in files] if files else [],"projects":projects})
+
+@app.get("/admin-section", response_class=HTMLResponse)
+def get_admin_pannel(request : Request,admin=Depends(get_admin)):
+    projects = all_project()
+    return templates.TemplateResponse("save_project.html", {"request":request,"projects":projects})
+
+
+@app.get("/project/{id}",response_class=HTMLResponse)
+def delete_project(request:Request,id : int):
+    with Session(engine) as session:
+        
+        choosen_project = session.get(Projects,id)
+        if choosen_project:
+            session.delete(choosen_project)
+            session.commit()
+            
+    return RedirectResponse(url="/admin-section",status_code=303)
+
+@app.get("/edit/project/{id}")
+def get_edit_project(request:Request,id:int):
+    with Session(engine) as session:
+        project = session.get(Projects,id)
+        
+    return templates.TemplateResponse("edit_project.html",{"request":request,"project":project})
+
+
+@app.post("/edit/project/{id}")
+def post_edit_project(request:Request,id:int,title: str = Form(...),description:str = Form(...),preview:str=Form(...)):
+    with Session(engine) as session:
+        project = session.get(Projects,id)
+        if not project:
+            return {"eror":"not found"}
+        
+        project.title = title
+        project.description = description
+        project.preview = preview
+        session.commit()
+        return RedirectResponse("/admin-section", status_code=303)
+        
     
-        # project = session.get(Projects, 2) 
-        # print(project.description)
-   
-       
-    return templates.TemplateResponse("save_project.html", {"request":request,"filename": [f.filename for f in files]})
-
-@app.get("/test", response_class=HTMLResponse)
-def get_test(request : Request):
-     return templates.TemplateResponse("save_project.html", {"request":request})
-
-
-
-
-@app.get("/user/admin")
-def admin(credentials : Annotated[HTTPBasicCredentials,Depends(security)]):
-        return {"username": credentials.username, "password": credentials.password}
-
+    return templates.TemplateResponse("edit_project.html",{"request":request,"project":project})
 
 
 def all_project():
@@ -76,6 +137,8 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def home(request : Request):
     projects = all_project()
+    
+    
     return templates.TemplateResponse("index.html",{"request":request, "projects" : projects})
 
 
@@ -94,7 +157,39 @@ def get_project(request : Request ,id : int):
     return templates.TemplateResponse("projects.html", {"request":request, "project" : project})
     
 
-@app.get("/login", response_class=HTMLResponse)
-def login(request: Request):
-    return templates.TemplateResponse("login.html", {"request":request})
+@app.post("/submit_contact",response_class=HTMLResponse)
+async def contact(request:Request):
+    form_data = await request.form()
+    data = dict(form_data)
+    await send_email(data)
+    return RedirectResponse(url="/#contact",status_code=303)
+
+
+
+async def send_email(data):
+    conf = ConnectionConfig(
+        MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+        MAIL_PASSWORD= os.getenv("MAIL_PASSWORD"),
+        MAIL_FROM= os.getenv("MAIL_FROM"),
+        MAIL_PORT=587,
+        MAIL_SERVER="smtp.gmail.com",
+        MAIL_SSL_TLS=False,
+        MAIL_STARTTLS=True,
+        )
     
+    message = MessageSchema(
+       subject="PORTFOLIO WEB  #PYTHON",
+       recipients= ["kuba.vecera@email.cz"],
+       body=f"Email z tvého webu:\n\n"
+       f"JMÉNO: {data["name"]}, EMAIL: {data["email"]}\n\n"
+       "ZPRÁVA:\n\n"
+       f"{data["msg"]}",
+       subtype="plain"
+       )
+    
+    
+    fm = FastMail(conf)
+    await fm.send_message(message)
+    print("message sent")
+
+
