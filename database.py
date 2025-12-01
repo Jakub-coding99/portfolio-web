8d9685372a08dcfa1a06234e70e6b18273b94857
Jakub Večeřa <kuba.vecera@email.cz>
2025-12-01 12:59:13 +0100

    setting db engine to not fall on 500 error

--- database.py
+++ database.py
@@ -37,7 +37,12 @@ class Admin(SQLModel, table = True):
 DATABASE_URL = os.getenv("DB_URL")
 
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL,
+                        echo=True,
+                        max_overflow=10,
+                        pool_pre_ping=True,
+                        pool_size=5,
+                        pool_recycle=1800)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
